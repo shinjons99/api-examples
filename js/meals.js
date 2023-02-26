ce0bcd8ced09d1f4ccc425b0664501223b825514
shinjons99 <shinjons99@gmail.com>
2023-02-26 13:01:30 +0600

    Display dynamic search result using bootstrap cards

--- js/meals.js
+++ js/meals.js
@@ -1,13 +1,15 @@
-const loadMeals =()=>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+const loadMeals =(searchText)=>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals));
 }
 
 const displayMeals = meals =>{
-    // console.log(meals);
+    console.log(meals);
     //* step-1: container Element
     const mealsContainer = document.getElementById('meals-container');
+    mealsContainer.innerHTML = '';
     meals.forEach(meal =>{
         console.log(meal)
         //*step-2: create child for each Element 
@@ -16,9 +18,9 @@ const displayMeals = meals =>{
         mealDiv.classList.add('col');
         mealDiv.innerHTML= `
             <div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
             </div>
@@ -32,4 +34,12 @@ const displayMeals = meals =>{
     })
 }
 
-loadMeals();
+const searchMeals = () =>{
+    const searchText = document.getElementById('search-field').value;
+    //* search meals
+    console.log(searchText);
+    loadMeals(searchText);
+        
+}
+
+loadMeals('rice');
